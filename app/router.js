018d078ea046f1bc207d5d4ea9ec1026fce780f4
柴逸宁 <chaiyining@wansecheng.com>
2017-11-20 15:09:08 +0800

    根据约定 更新文件

--- app/router.js
+++ app/router.js
@@ -1,5 +1,5 @@
 'use strict';
 const base='/api'
 module.exports = app => {
-  app.resources('task', `${base}/task`, app.controller.task);
+  app.resources('task', `${base}/task`, app.controller.api.tasks);
 };
